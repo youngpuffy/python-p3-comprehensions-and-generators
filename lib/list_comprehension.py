49baa438d31c446011bd07c0e7bca176e45442ca
Youngpuffy <levimuturi2@gmail.com>
2025-05-13 08:12:25 +0300

    comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
     pass
 
 def make_exclamation(sentence_list):
+    if not sentence_list:
+        return []
+    return[sentence + '!' for sentence in sentence_list]
     pass
